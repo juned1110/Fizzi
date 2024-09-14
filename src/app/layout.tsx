167f43ff1a51e6022f7c0476506e65f790f21bf3
juned1110 <junedking1110@gmail.com>
2024-09-14 13:32:04 +0530

    canvas added in layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+import "./app.css";
+import Header from "@/components/Header";
+import ViewCanvas from "@/components/ViewCanvas";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={alpino.variable}>
+      <body className="overflow-x-hidden bg-yellow-400">
+        <Header />
+        <main>{children}</main>
+        <ViewCanvas />
+      </body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
