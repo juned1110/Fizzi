3a8ce03c2c8d1787438efe1eb7c1326589951a8f
juned1110 <junedking1110@gmail.com>
2024-09-24 13:59:22 +0530

    carousel, big_text, a_lternating_text added

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -3,6 +3,9 @@
 import dynamic from "next/dynamic";
 
 export const components = {
+  a_lternating_text: dynamic(() => import("./ALternatingText")),
+  big_text: dynamic(() => import("./BigText")),
+  carousel: dynamic(() => import("./Carousel")),
   hero: dynamic(() => import("./Hero")),
   sky_dive: dynamic(() => import("./SkyDive")),
 };
